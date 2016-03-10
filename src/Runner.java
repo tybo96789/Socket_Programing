0195465c6630e4ce2f000dcfab63c310b1bbf43e
Tyler Atiburcio <tyler.atiburcio@gmail.com>
2016-03-09 15:56:40 -1000

    Runner complete

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,55 @@
+
+import client.Client;
+import server.Server;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author tba_m
  */
 public class Runner {
+    
+    public static void main(String[] args) {
+        if(args.length == 0)
+        {
+            new Client();
+        }
+        switch(args[0].trim().toLowerCase())
+        {
+            case "server":
+            case "s":
+            {
+                if(args[1] != null)
+                {
+                    try{
+                       new Server(InetAddress.getByName(args[1]));
+                    }catch(UnknownHostException e)
+                    {
+                        System.err.println(e.toString());
+                        try {
+                            new Server(InetAddress.getLocalHost());
+                        } catch (UnknownHostException ex) {
+                            System.err.println("Unable to bind to localhost!");
+                            System.exit(0);
+                        }
+                    }
+                    
+                }
+                break;
+            }
+            case "client":
+            case "c":
+            {
+                new Client();
+                break;
+            }
+            default:
+            {
+                System.err.println("Unknown Command. Terminating");
+                System.exit(0);
+            }
+        }
+    }
 
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,9 +1,18 @@
 package server;
 
+import java.net.InetAddress;
+
 /**
  *
  * @author tba_m
  */
 public class Server {
+    final InetAddress inetAddress;
+
+    public Server(InetAddress inetAddress) {
+        this.inetAddress = inetAddress;
+    }
+    
+    
 
 }
