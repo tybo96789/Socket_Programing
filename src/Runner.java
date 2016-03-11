a55a8662b1ba66a9d60bb39b6012cc8dac97585d
Tyler A <tyler.atiburcio@gmail.com>
2016-03-10 20:53:11 -1000

    Server/Client Created. Send/Receive broken

--- src/Runner.java
+++ src/Runner.java
@@ -13,35 +13,56 @@ public class Runner {
     public static void main(String[] args) {
         if(args.length == 0)
         {
-            new Client();
+            try {
+                new Client(InetAddress.getLocalHost(),80);
+            } catch (UnknownHostException ex) {
+                System.err.println("Unable to get Localhost");
+                System.exit(1);
+            }
         }
         switch(args[0].trim().toLowerCase())
         {
             case "server":
             case "s":
+            case "-server":
+            case "-s":
             {
-                if(args[1] != null)
+                try{
+                   new Server(InetAddress.getByName(args[1]));
+                }catch(Exception e)
                 {
-                    try{
-                       new Server(InetAddress.getByName(args[1]));
-                    }catch(UnknownHostException e)
-                    {
-                        System.err.println(e.toString());
-                        try {
-                            new Server(InetAddress.getLocalHost());
-                        } catch (UnknownHostException ex) {
-                            System.err.println("Unable to bind to localhost!");
-                            System.exit(0);
-                        }
+                    System.err.println(e.toString());
+                    try {
+                        new Server(InetAddress.getLocalHost());
+                    } catch (UnknownHostException ex) {
+                        System.err.println("Unable to bind to localhost!");
+                        System.exit(0);
                     }
-                    
                 }
+                    
+
                 break;
             }
             case "client":
             case "c":
+            case "-client":
+            case "-c":
             {
-                new Client();
+
+                try{
+                   new Client(InetAddress.getByName(args[1]),Integer.parseInt(args[2]));
+                }catch(UnknownHostException e)
+                {
+                    System.err.println(e.toString());
+                    try {
+                        new Server(InetAddress.getLocalHost());
+                    } catch (Exception ex) {
+                        System.err.println("Unable to bind to localhost!");
+                        System.exit(0);
+                    }
+                }
+                    
+
                 break;
             }
             default:
