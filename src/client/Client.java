563fd2d3c9a694c43f2fb224c1027eaa9b6d7522
Tyler A <tyler.atiburcio@gmail.com>
2016-03-11 20:26:28 -1000

    Switched to DataOutputStream
    As shown by powerpoint

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,6 +1,7 @@
 package client;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -20,11 +21,11 @@ public class Client {
         this.port = parseInt;
         
         Socket socks = null;
-        PrintWriter out = null;
+        DataOutputStream out = null;
         BufferedReader in = null;
         try{
             socks = new Socket(this.address,this.port);
-            out = new PrintWriter(socks.getOutputStream(),true);
+            out = new DataOutputStream(socks.getOutputStream());
             in = new BufferedReader(new InputStreamReader(socks.getInputStream()));
         }catch(IOException e)
         {
@@ -32,15 +33,23 @@ public class Client {
             System.exit(1);
         }
         
-        System.out.println("Connected...");
+        System.out.println("Connected @ " + socks.getInetAddress() +":"+ socks.getPort() );
         
         try{
-            String data;
-            while((data = in.readLine()) != null)
+            while(true)
             {
-                out.print("Aloha");
-                System.out.println(data);
+                System.out.println(in.readLine());
             }
+            
+//            boolean isDone = false;
+//            String data = null;
+//            while(true && !isDone)
+//            {
+//                data = in.readLine();
+//                System.out.println(data);
+//                out.writeBytes("Aloha");
+//                isDone = true;
+//            }
             
             
         }catch(Exception e)
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,6 +1,7 @@
 package server;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -37,11 +38,11 @@ public class Server {
             System.exit(1);
         }
         
-        System.out.println("Connection Established");
-        PrintWriter out = null;
+        System.out.println("Connection Established @" + clientSocks.getInetAddress() +":" + clientSocks.getPort());
+        DataOutputStream out = null;
         BufferedReader in = null;
         try{
-        out = new PrintWriter(clientSocks.getOutputStream(),true);
+        out = new DataOutputStream(clientSocks.getOutputStream());
         in = new BufferedReader( new InputStreamReader(clientSocks.getInputStream()));
         
         }catch(IOException e)
@@ -50,18 +51,23 @@ public class Server {
             System.exit(1);
         }
         
-        String data;
+        String data = null;
         boolean isDone = false;
         try{
-        while((data = in.readLine()) != null && !isDone)
-        {
-            System.out.println(data);
-            if(data.equalsIgnoreCase("aloha"))
+            while(true)
             {
-                out.print("Hello");
-                
+                out.writeBytes("Hello");
             }
-        }
+//        while(true && !isDone)
+//        {
+//            data = in.readLine();
+//            System.out.println(data);
+//            if(data.equalsIgnoreCase("aloha"))
+//            {
+//                out.writeBytes("Hello");
+//                
+//            }
+//        }
         }catch(IOException e)
         {
             System.err.println("Socket IO error");
